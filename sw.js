6dc6a3989d6c0067f893a4d0878c4bd4e78cf32f
elohim-koffi <elohimk98@gmail.com>
2026-05-22 16:39:19 +0000

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'cave-laliga-v3';
+const urlsToCache = [
+  './magasin.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
