92fcd1c4f276528eb93486c88af68d0fe73921ba
elohim-koffi <elohimk98@gmail.com>
2026-05-22 14:02:35 +0000

    Create presentation.py

---
+++ presentation.py
@@ -0,0 +1,16 @@
+# Mon premier projet Python pour les bourses 2027
+# Par Koffi Kouadio Salomon Eloïm - première D
+
+nom = "Koffi Kouadio Salomon Eloïm"
+classe = "Terminale D"
+lycee = "Lycée Moderne 1 de Toumodi"
+moyenne = 14.06
+objectif = "Bourse Stipendium Hungaricum ou CSC Chine 2027 pour étudier l'IA"
+
+print("=" * 55)
+print(f"Salut, moi c'est Koffi Kouadio Salomon Elohïm 👋")
+print(f"Je suis en de première D au lycée Moderne 1 de toumodi ")
+print(f"Ma moyenne de 1ère : 14,06/20")
+print(f"Mon rêve : devenir Un grand ingénieur en informatique et vendre l'image de mon pays dans le monde entier car l'informatique est une passion pour moi depuis l'enfance ")
+print("=" * 55)
+print("Ce GitHub est le début de mon parcours vers l'IA !")
